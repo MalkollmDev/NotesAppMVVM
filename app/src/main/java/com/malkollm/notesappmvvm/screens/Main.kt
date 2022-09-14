376e62e916984dc0f6711b4880eb2e2ebab4016c
Marsel <maergon@yandex.ru>
2022-09-14 16:27:52 +0500

    Added Jetpack Navigation

---
+++ app/src/main/java/com/malkollm/notesappmvvm/screens/Main.kt
@@ -0,0 +1,10 @@
+package com.malkollm.notesappmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.malkollm.notesappmvvm.navigation.NavRoute
+
+@Composable
+fun Main(navController: NavHostController) {
+
+}
